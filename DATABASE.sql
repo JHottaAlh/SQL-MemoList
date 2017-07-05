7569a6e12fd18820b3b0768d5390662222125132
Smi <alhinc.hotta.jun@gmail.com>
2017-07-05 10:14:26 +0900

    Update DATABASE.sql

--- DATABASE.sql
+++ DATABASE.sql
@@ -5,3 +5,12 @@ CREATE DATABASE DB_name;
 
 #使用するDBを宣言
 USE DB_name;
+
+cmdでのデータベースのdump(他データベースで複製する場合やバックアップが可能)
+../../
+
+cs "Program Files\MySQL\MySQL Server 5.6\bin"
+
+mysqldump -u root -p database_name > xxx.sql
+
+Enter password: *********
